4c9190622ea272bdfc91df7f4f3b86f182338a0d
Omar Baltaji <omar.baltaji@app4legal.com>
2023-02-26 17:05:06 +0200

    hide the follow/unfollow button in case the auth user is the same as the author. Also, fixed action for the form to ensure the endpoint starts with /follow

--- resources/views/components/author-section.blade.php
+++ resources/views/components/author-section.blade.php
@@ -9,9 +9,9 @@
       
       @php $is_auth_a_follower = in_array(auth()->user()->id, $post->author->followers->pluck('id')->toArray()); @endphp
       
-      <button x-data="{}" @click.prevent="document.querySelector('#follow-form').submit()" class="bg-blue-500 px-4 py-2 text-white rounded-3xl text-xs {{ $is_auth_a_follower ? 'opacity-75' : '' }}">{{  $is_auth_a_follower ? 'Unfollow' : 'Follow' }}</button>
+      <button x-data="{}" @click.prevent="document.querySelector('#follow-form').submit()" class="bg-blue-500 px-4 py-2 text-white rounded-3xl text-xs {{ auth()->user()->id === $post->author->id ? 'hidden' : '' }} {{ $is_auth_a_follower ? 'opacity-75' : '' }}">{{  $is_auth_a_follower ? 'Unfollow' : 'Follow' }}</button>
       
-      <form id="follow-form" action="follow/{{ $post->author->id }}" method="POST" class="hidden">
+      <form id="follow-form" action="/follow/{{ $post->author->id }}" method="POST" class="hidden">
           @csrf
       </form>
   </div>
