9cac6c67f39a2fe3e1e04564cd2f5bf10e3ed08e
Omar Baltaji <omar.baltaji@app4legal.com>
2023-02-27 20:40:42 +0200

    fixed error when user is not logged in

--- resources/views/components/author-section.blade.php
+++ resources/views/components/author-section.blade.php
@@ -7,9 +7,9 @@
           <a href="/?author={{ $post->author->username }}">{{ ucwords($post->author->name) }}</a>
       </h5>
       
-      @php $is_auth_a_follower = in_array(auth()->user()->id, $post->author->followers->pluck('id')->toArray()); @endphp
+      @php $is_auth_a_follower = in_array(auth()->user()?->id, $post->author->followers->pluck('id')->toArray()); @endphp
       
-      <button x-data="{}" @click.prevent="document.querySelector('#follow-form').submit()" class="bg-blue-500 px-4 py-2 text-white rounded-3xl text-xs {{ auth()->user()->id === $post->author->id ? 'hidden' : '' }} {{ $is_auth_a_follower ? 'opacity-75' : '' }}">{{  $is_auth_a_follower ? 'Unfollow' : 'Follow' }}</button>
+      <button x-data="{}" @click.prevent="document.querySelector('#follow-form').submit()" class="bg-blue-500 px-4 py-2 text-white rounded-3xl text-xs {{ auth()->user()?->id === $post->author->id ? 'hidden' : '' }} {{ $is_auth_a_follower ? 'opacity-75' : '' }}">{{  $is_auth_a_follower ? 'Unfollow' : 'Follow' }}</button>
       
       <form id="follow-form" action="/follow/{{ $post->author->id }}" method="POST" class="hidden">
           @csrf
