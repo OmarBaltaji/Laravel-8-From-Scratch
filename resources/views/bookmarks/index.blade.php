0ae1c2a363aa4983e3a5921d15ff4a8848673da1
Omar Baltaji <omar.baltaji@app4legal.com>
2023-02-28 22:28:39 +0200

    added text in case no bookmarks exist

--- resources/views/bookmarks/index.blade.php
+++ resources/views/bookmarks/index.blade.php
@@ -2,7 +2,8 @@
   <section class="py-8 max-w-4xl mx-auto">
     <h1 class="text-lg font-bold mb-8 border-b pb-2">Bookmarks</h1>
     <div class="flex flex-col">
-        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      @if (!$bookmarks->isEmpty())
+          <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                     <table class="min-w-full divide-y divide-gray-200">
@@ -24,7 +25,10 @@
                     </table>
                 </div>
             </div>
-        </div>
+          </div>
+      @else 
+        No bookmarks yet
+      @endif
     </div>
   </section>
 </x-layout>
